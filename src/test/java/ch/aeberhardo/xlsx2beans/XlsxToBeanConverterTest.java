3f8560c57549565d4b944186e69f3f38e15c6c1d
aeberhardo <aeberhard@gmx.ch>
2013-06-30 10:13:47 +0200

    testing empty cells in defined cols get ignored

--- src/test/java/ch/aeberhardo/xlsx2beans/XlsxToBeanConverterTest.java
+++ src/test/java/ch/aeberhardo/xlsx2beans/XlsxToBeanConverterTest.java
@@ -19,7 +19,7 @@ public class XlsxToBeanConverterTest {
 	}
 
 	@Test
-	public void testConvert() throws ParseException {
+	public void test_convert() throws ParseException {
 
 		XlsxToBeanConverter converter = new XlsxToBeanConverter();
 
@@ -37,6 +37,28 @@ public class XlsxToBeanConverterTest {
 		assertEquals("This is my string 2", beans.get(1).getMyString2());
 		assertEquals(Integer.valueOf(456), beans.get(1).getMyInteger());
 		assertEquals(Double.valueOf(9.87), beans.get(1).getMyDouble());
+		assertEquals(new SimpleDateFormat("dd.MM.yyyy hh:mm:ss").parse("20.06.2011 23:50:33"), beans.get(1).getMyDate());
+	}
+
+	@Test
+	public void test_convertWithEmptyCells() throws ParseException {
+
+		XlsxToBeanConverter converter = new XlsxToBeanConverter();
+
+		List<TestBean1> beans = converter.convert(getClass().getResourceAsStream("/test-valid_empty_cells.xlsx"), 0, TestBean1.class);
+
+		assertEquals(2, beans.size());
+
+		assertEquals("ABC", beans.get(0).getMyString1());
+		assertEquals(null, beans.get(0).getMyString2());
+		assertEquals(Integer.valueOf(123), beans.get(0).getMyInteger());
+		assertEquals(Double.valueOf(7.89), beans.get(0).getMyDouble());
+		assertEquals(null, beans.get(0).getMyDate());
+
+		assertEquals("DEF", beans.get(1).getMyString1());
+		assertEquals("This is my string 2", beans.get(1).getMyString2());
+		assertEquals(Integer.valueOf(456), beans.get(1).getMyInteger());
+		assertEquals(null, beans.get(1).getMyDouble());
 		assertEquals(new SimpleDateFormat("dd.MM.yyyy hh:mm:ss").parse("20.06.2011 23:50:33"), beans.get(1).getMyDate());
 	}
 
