25bf09d55a701660caae9db95dff7544db305165
aeberhardo <aeberhard@gmx.ch>
2013-07-12 08:45:06 +0200

    added test for empty rows

--- src/test/java/ch/aeberhardo/xlsx2beans/parser/XlsxSheetParserTest.java
+++ src/test/java/ch/aeberhardo/xlsx2beans/parser/XlsxSheetParserTest.java
@@ -56,6 +56,34 @@ public class XlsxSheetParserTest {
 	}
 
 	@Test
+	public void test_emptyRow() {
+
+		try (OPCPackage pkg = OPCPackage.open(getClass().getResourceAsStream("/test-valid_with_empty_row.xlsx"))) {
+
+			XSSFWorkbook wb = new XSSFWorkbook(pkg);
+			XSSFSheet sheet = wb.getSheetAt(0);
+
+			XlsxSheetParser parser = new XlsxSheetParser();
+
+			XlsxSheetEventHandler handlerMock = mock(XlsxSheetEventHandler.class);
+
+			parser.parse(sheet, handlerMock);
+
+			// Test sheet contains 3 rows.
+			verify(handlerMock, times(3)).startRow(anyInt());
+
+			// Row 3 is empty.
+			verify(handlerMock).startRow(1);
+			verify(handlerMock).startRow(2);
+			verify(handlerMock).startRow(4);
+
+		} catch (InvalidFormatException | IOException e) {
+			throw new RuntimeException(e);
+		}
+
+	}
+
+	@Test
 	public void test_invalidHeader() {
 
 		try (OPCPackage pkg = OPCPackage.open(getClass().getResourceAsStream("/test-invalid_header.xlsx"))) {
@@ -81,50 +109,50 @@ public class XlsxSheetParserTest {
 
 	@Test
 	public void test_nonUniqueHeader() {
-		
+
 		try (OPCPackage pkg = OPCPackage.open(getClass().getResourceAsStream("/test-non_unique_header.xlsx"))) {
-			
+
 			XSSFWorkbook wb = new XSSFWorkbook(pkg);
 			XSSFSheet sheet = wb.getSheetAt(0);
-			
+
 			XlsxSheetParser parser = new XlsxSheetParser();
-			
+
 			XlsxSheetEventHandler handlerMock = mock(XlsxSheetEventHandler.class);
-			
+
 			parser.parse(sheet, handlerMock);
 			fail("Expected exception was not thrown!");
-			
+
 		} catch (XlsxParserException e) {
 			assertEquals("Error while parsing header (rowNum=0, colIndex=3): The column header with name 'MyString2' is not unique!", e.getMessage());
-			
+
 		} catch (InvalidFormatException | IOException e) {
 			throw new RuntimeException(e);
 		}
-		
+
 	}
-	
+
 	@Test
 	public void test_undefinedHeader() {
-		
+
 		try (OPCPackage pkg = OPCPackage.open(getClass().getResourceAsStream("/test-header_not_definied.xlsx"))) {
-			
+
 			XSSFWorkbook wb = new XSSFWorkbook(pkg);
 			XSSFSheet sheet = wb.getSheetAt(0);
-			
+
 			XlsxSheetParser parser = new XlsxSheetParser();
-			
+
 			XlsxSheetEventHandler handlerMock = mock(XlsxSheetEventHandler.class);
-			
+
 			parser.parse(sheet, handlerMock);
 			fail("Expected exception was not thrown!");
-			
+
 		} catch (XlsxParserException e) {
 			assertEquals("Error while parsing cell (rowNum=1, colIndex=5): No header name defined!", e.getMessage());
-			
+
 		} catch (InvalidFormatException | IOException e) {
 			throw new RuntimeException(e);
 		}
-		
+
 	}
 
 }
