830611cefe43680f1ff9deffa1c65168c87c9f46
aeberhardo <aeberhard@gmx.ch>
2013-07-12 08:52:38 +0200

    Comments on facade XlsxToBeanConverter

--- src/main/java/ch/aeberhardo/xlsx2beans/XlsxToBeanConverter.java
+++ src/main/java/ch/aeberhardo/xlsx2beans/XlsxToBeanConverter.java
@@ -15,6 +15,16 @@ import ch.aeberhardo.xlsx2beans.parser.XlsxSheetParser;
 
 public class XlsxToBeanConverter {
 
+	/**
+	 * Parse a XLSX spread sheet stream and convert rows to a list of beans.
+	 * The first row of the sheet has to contain the names of the bean properties to be mapped.
+	 * Mapping is done by applying annotation on the target bean class.
+	 * 
+	 * @param xlsxWorkbookInputStream The XLSX spread sheet input stream to be converted.
+	 * @param sheetIndex Index of the spead sheet tab starting with 0.
+	 * @param beanType The type of class the spread sheet rows get converted to.
+	 * @return A list of newly created beans. Every spread sheet row gets converted to a single bean instance.
+	 */
 	public <T> List<T> convert(InputStream xlsxWorkbookInputStream, int sheetIndex, Class<T> beanType) {
 
 		try (OPCPackage pkg = OPCPackage.open(xlsxWorkbookInputStream)) {
@@ -29,6 +39,15 @@ public class XlsxToBeanConverter {
 		}
 	}
 
+	/**
+	 * Parse a XLSX spread sheet stream and convert rows to a list of beans.
+	 * The first row of the sheet has to contain the names of the bean properties to be mapped.
+	 * Mapping is done by applying annotation on the target bean class.
+	 * 
+	 * @param sheet The spead sheet to be converted.
+	 * @param beanType The type of class the spread sheet rows get converted to.
+	 * @return A list of newly created beans. Every spread sheet row gets converted to a single bean instance.
+	 */
 	public <T> List<T> convert(XSSFSheet sheet, Class<T> beanType) {
 
 		XlsxToBeanConverterHandler<T> handler = new XlsxToBeanConverterHandler<T>(beanType);
